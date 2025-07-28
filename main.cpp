6994c4807f6bcd7dcde47ed895a5bbbdc753a9ad
Nekochan <jan.mierzejewski0@gmail.com>
2025-07-28 14:36:26 +0200

    Made it a 2D transport, but some instabilities arise for solve

--- main.cpp
+++ main.cpp
@@ -28,10 +28,25 @@ std::vector<arma::cx_double> Energy(int N, double E_min, double dE, const double
     return Energy;
 }
 
-arma::cx_mat hamiltonian_1D(size_t len, arma::cx_mat t)
+arma::cx_mat hamiltonian(size_t xsize, arma::cx_mat h, arma::cx_mat u)
 {
     arma::cx_mat Hamiltonian;
-    arma::cx_mat H_I = 2*arma::eye<arma::cx_mat>(len, len);
+    arma::cx_mat H_I = arma::eye<arma::cx_mat>(xsize, xsize);
+    arma::cx_mat H_down = arma::zeros<arma::cx_mat>(xsize, xsize);
+    arma::cx_mat H_up = arma::zeros<arma::cx_mat>(xsize, xsize);
+    for(size_t i = 0; i<xsize-1; i++)
+    {
+        H_down(i+1,i) = 1;
+        H_up(i,i+1) = 1;
+    }
+    Hamiltonian = arma::kron(H_I, h) + arma::kron(H_down, u) + arma::kron(H_up, u);
+    return Hamiltonian;
+}
+
+arma::cx_mat block_matrix(size_t len, arma::cx_double node, arma::cx_double t)
+{
+    arma::cx_mat block;
+    arma::cx_mat H_I = arma::eye<arma::cx_mat>(len, len);
     arma::cx_mat H_down = arma::zeros<arma::cx_mat>(len, len);
     arma::cx_mat H_up = arma::zeros<arma::cx_mat>(len, len);
     for(size_t i = 0; i<len-1; i++)
@@ -39,9 +54,10 @@ arma::cx_mat hamiltonian_1D(size_t len, arma::cx_mat t)
         H_down(i+1,i) = -1;
         H_up(i,i+1) = -1;
     }
-    Hamiltonian = arma::kron(H_I, t) + arma::kron(H_down, t) + arma::kron(H_up, t);
-    return Hamiltonian;
+    block = node*H_I + t*H_down + t*H_up;
+    return block;
 }
+
 
 
 /*------------Sancho-Rubio algorithm--------------
@@ -54,11 +70,9 @@ The function needs following inputs:
     -cx_mat t - a matrix that posesses the hopping integrals of the system.
 */
 
-std::vector<arma::cx_mat> sancho_rubio_algorithm(std::vector<arma::cx_double> Energy, arma::cx_mat t, double tol = 1e-8, int max_iter = 10000)
+std::vector<arma::cx_mat> sancho_rubio_algorithm(std::vector<arma::cx_double> Energy, arma::cx_mat h, arma::cx_mat u, double tol = 1e-8, long int max_iter = 1000000)
 {
-    arma::cx_mat u = -t;
     arma::cx_mat g_surface_i;
-    arma::cx_mat h;
     arma::cx_mat alpha;
     arma::cx_mat beta;
     arma::cx_mat epsilon;
@@ -70,7 +84,6 @@ std::vector<arma::cx_mat> sancho_rubio_algorithm(std::vector<arma::cx_double> En
     int iter;
     for(size_t i =0; i< Energy.size(); i++)
     {
-        h = 2*t;
         tmp = arma::solve(Energy[i]*arma::eye<arma::cx_mat>(u.n_rows, u.n_cols)-h, arma::eye<arma::cx_mat>(h.n_rows, h.n_cols));
         alpha = u*tmp*u;
         beta = arma::trans(u)*tmp*arma::trans(u);
@@ -103,35 +116,45 @@ std::vector<arma::cx_mat> sancho_rubio_algorithm(std::vector<arma::cx_double> En
 }
 
 
-std::vector<arma::cx_mat> retarted_GF(std::vector<arma::cx_double> Energy,  std::vector<arma::cx_mat> self_energy_left, std::vector<arma::cx_mat> self_energy_right, arma::cx_mat Hamiltonian)
+std::vector<arma::cx_mat> retarted_GF(size_t xsize, std::vector<arma::cx_double> Energy,  std::vector<arma::cx_mat> self_energy_left, std::vector<arma::cx_mat> self_energy_right, arma::cx_mat Hamiltonian)
 {
     arma::cx_mat mat_2_inv;
     arma::cx_mat tmp;
     std::vector<arma::cx_mat> GF_retarted;
     arma::cx_mat H;
+    arma::cx_mat self_energies_left;
+    arma::cx_mat self_energies_right;
+    
     for(size_t i = 0; i< Energy.size(); i++)
     {
-        H=Hamiltonian;
-        H(0,0) += self_energy_left[i](0,0);
-        H(Hamiltonian.n_rows-1, Hamiltonian.n_cols-1) += self_energy_right[i](0,0);
+        self_energies_left = arma::zeros<arma::cx_mat>(xsize, xsize);
+        self_energies_left(0,0) = 1;
+        self_energies_left = arma::kron(self_energies_left, self_energy_left[i]);
+
+        self_energies_right = arma::zeros<arma::cx_mat>(xsize, xsize);
+        self_energies_right(xsize-1, xsize-1) = 1;
+        self_energies_right = arma::kron(self_energies_right, self_energy_right[i]);
+
+        H=Hamiltonian+self_energies_left+self_energies_right;
+
         mat_2_inv = Energy[i]*arma::eye<arma::cx_mat>(H.n_rows, H.n_cols) - H;
-        tmp = arma::solve(mat_2_inv, arma::eye<arma::cx_mat>(mat_2_inv.n_rows, mat_2_inv.n_cols));
+        tmp = arma::solve(mat_2_inv, arma::eye<arma::cx_mat>(mat_2_inv.n_rows, mat_2_inv.n_cols), arma::solve_opts::likely_sympd);
         GF_retarted.push_back(tmp);
     }
     return GF_retarted;
 }
 
-std::vector<arma::cx_double> Transport_function(std::vector<arma::cx_mat> retarded_Greens_function, std::vector<arma::cx_mat> gamma_left, std::vector<arma::cx_mat> gamma_right)
+std::vector<arma::cx_double> Transmission_function(size_t xsize, std::vector<arma::cx_mat> retarded_Greens_function, std::vector<arma::cx_mat> gamma_left, std::vector<arma::cx_mat> gamma_right)
 {
-    std::vector<arma::cx_double> transport_function;
+    std::vector<arma::cx_double> transmission_function;
     arma::cx_mat GF_advanced;
     for(size_t i = 0; i<retarded_Greens_function.size(); i++)
     {
         GF_advanced = arma::trans(retarded_Greens_function[i]);
-        transport_function.push_back(arma::trace(GF_advanced(1,GF_advanced.n_rows-1)*gamma_right[i]*retarded_Greens_function[i](1, retarded_Greens_function[i].n_cols-1)*gamma_left[i]));
+        transmission_function.push_back(arma::trace(GF_advanced.submat(0, 0, xsize-1, xsize-1)*gamma_right[i]*retarded_Greens_function[i].submat(0, 0, xsize-1, xsize-1)*gamma_left[i]));
         
     }
-    return transport_function;
+    return transmission_function;
 }
 
 void ldos(std::vector<arma::cx_mat>& spectral_function, std::vector<arma::cx_double>& density_of_states, std::vector<arma::cx_mat> retarted_Greens_functions)
@@ -153,25 +176,28 @@ int main()
 {
     
     //initial values required for the energy
-    double E_min = -1;            //minimum energy in eV
+    double E_min = -3;            //minimum energy in eV
     const double eta = 1e-18;       //a small constant approaching zero that models discontinuity in eV
     double dE = 0.001;               //the difference between next and previous energy eV    
-    int N = 6/dE;                   //number of points calculated
-    size_t size = 10;
+    int N = 9/dE;                   //number of points calculated
+    size_t ysize = 3;
+    size_t xsize = 3;
 
 
     //Assaining an energy
     std::vector<arma::cx_double> E = Energy(N, E_min, dE, eta);     //energy data of the system in eV
     
     //calculating the aproximate surface Green's function
-    arma::cx_mat t = arma::ones<arma::cx_mat>(1, 1);    //matrix containing hopping integrals
-    t(0, 0) = 1;
+    arma::cx_double t = arma::cx_double(1, 0);    //matrix containing hopping integrals
+    arma::cx_mat h = block_matrix(ysize, arma::cx_double(2,0)*t, -t);
+    arma::cx_mat u = block_matrix(ysize, -t, 0);
 
-    std::vector<arma::cx_mat> g_surface = sancho_rubio_algorithm(E, t);
+
+    std::vector<arma::cx_mat> g_surface = sancho_rubio_algorithm(E, h, u);
 
     arma::cx_double ii = arma::cx_double(0,1);
 
-    arma::cx_mat u = -t;
+    
     std::vector<arma::cx_mat> self_energy_left;
     std::vector<arma::cx_mat> self_energy_right;
     std::vector<arma::cx_mat> gamma_left;
@@ -184,9 +210,9 @@ int main()
         gamma_right.push_back(ii*(self_energy_right[i] - arma::trans(self_energy_right[i])));
     }
 
-    arma::cx_mat H_sample = hamiltonian_1D(size, t);
+    arma::cx_mat H_sample = hamiltonian(xsize, h, u);
     
-    std::vector<arma::cx_mat> GF_retarted = retarted_GF( E, self_energy_left, self_energy_right, H_sample);
+    std::vector<arma::cx_mat> GF_retarted = retarted_GF(xsize, E, self_energy_left, self_energy_right, H_sample);
 
     std::vector<arma::cx_mat> GF_advanced;
     
@@ -195,10 +221,10 @@ int main()
         GF_advanced.push_back(arma::trans(GF_retarted[i]));
     }
 
-    std::vector<arma::cx_double> T = Transport_function(GF_retarted, gamma_left, gamma_right);
+    std::vector<arma::cx_double> T = Transmission_function(xsize, GF_retarted, gamma_left, gamma_right);
 
     double e = 1;
-    double h = 4.135667662e-15;
+    double h_planck = 4.135667662e-15;
 
     std::vector<arma::cx_mat> A;
 
@@ -209,152 +235,143 @@ int main()
     std::cout<<"Calculations ended succesfully\n";
 
     //Saving to file
-    std::fstream surf_greens_fun("surf_greens_fun.dat", std::ios::out);
-
-    if(!surf_greens_fun)
+    if(false)
     {
-        std::system("touch surf_greens_fun.dat");
-    }
+        std::fstream surf_greens_fun("surf_greens_fun.dat", std::ios::out);
 
-    if(!surf_greens_fun)
-    {
-        std::cerr << "File surf_greens_fun.dat was unable to open";
-        return 1;
-    }
-
-    
-    for(size_t i = 0; i<E.size(); i++)
-    {
-        surf_greens_fun << E[i].real() << " " << g_surface[i](0,0).real() << " " << g_surface[i](0,0).imag() << std::endl;
-    }
-    
-    surf_greens_fun.close();
-    std::cout << "Surface Greens function saved to a file succesfully\n";
-
-    std::fstream self_energies("self_energies.dat", std::ios::out);
-
-    if(!self_energies)
-    {
-        std::system("touch self_energies.dat");
-    }
-
-    if(!self_energies)
-    {
-        std::cerr << "File self_energies.dat was unable to open";
-        return 1;
-    }
-
-    
-    for(size_t i = 0; i<E.size(); i++)
-    {
-        self_energies << E[i].real() << " " << self_energy_left[i](0,0).real()  << " " << self_energy_left[i](0,0).imag() << " " << self_energy_right[i](0,0).real()  << " " << self_energy_right[i](0,0).imag() << std::endl;
-    }
-    
-    self_energies.close();
-    std::cout << "Self energies saved to a file succesfully\n";
-
-    std::fstream gamma("gamma.dat", std::ios::out);
-
-    if(!gamma)
-    {
-        std::system("touch gamma.dat");
-    }
-
-    if(!gamma)
-    {
-        std::cerr << "File gamma.dat was unable to open";
-        return 1;
-    }
-
-    
-    for(size_t i = 0; i<E.size(); i++)
-    {
-        gamma << E[i].real() << " " << gamma_left[i](0,0).real()  << " " << gamma_left[i](0,0).imag() << " " << gamma_right[i](0,0).real()  << " " << gamma_right[i](0,0).imag() << std::endl;
-    }
-    
-    gamma.close();
-    std::cout << "Gamma matrices saved to a file succesfully\n";
-
-    std::fstream hamiltonian_sample("hamiltonian_sample.dat", std::ios::out);
-
-    if(!hamiltonian_sample)
-    {
-        std::system("touch hamiltonian_sample.dat");
-    }
-
-    if(!hamiltonian_sample)
-    {
-        std::cerr << "File hamiltonian_sample.dat was unable to open";
-    }
-
-    for(size_t i = 0; i<size; i++)
-    {
-        for(size_t j = 0; j<size; j++)
+        if(!surf_greens_fun)
         {
-            hamiltonian_sample << H_sample(i, j) << " ";
+            std::system("touch surf_greens_fun.dat");
         }
-        hamiltonian_sample << std::endl;
-    }
 
-    hamiltonian_sample.close();
-    std::cout << "Hamiltonian of the sample saved to a file succesfully\n";
-
-    std::fstream Greens_fun_retarded("Greens_fun_retarded.dat", std::ios::out);
-
-    if(!Greens_fun_retarded)
-    {
-        std::system("touch Greens_fun_retarded.dat");
-    }
-
-    if(!Greens_fun_retarded)
-    {
-        std::cerr << "File Greens_fun_retarded.dat was unable to open";
-    }
-    /*
-    for(size_t k = 0; k< GF_retarted.size(); k++)
-    {
-        Greens_fun_retarded << "Retarded Green's function for E=" << E[k] << std::endl;
-        for(size_t i = 0; i<size; i++)
+        if(!surf_greens_fun)
         {
-            for(size_t j = 0; j<size; j++)
+            std::cerr << "File surf_greens_fun.dat was unable to open";
+            return 1;
+        }
+
+    
+        for(size_t i = 0; i<E.size(); i++)
+        {
+            surf_greens_fun << E[i].real() << " " << g_surface[i](0,0).real() << " " << g_surface[i](0,0).imag() << std::endl;
+        }
+    
+        surf_greens_fun.close();
+        std::cout << "Surface Greens function saved to a file succesfully\n";
+
+        std::fstream self_energies("self_energies.dat", std::ios::out);
+
+        if(!self_energies)
+        {
+            std::system("touch self_energies.dat");
+        }
+
+        if(!self_energies)
+        {
+            std::cerr << "File self_energies.dat was unable to open";
+            return 1;
+        }
+
+    
+        for(size_t i = 0; i<E.size(); i++)
+        {
+            self_energies << E[i].real() << " " << self_energy_left[i](0,0).real()  << " " << self_energy_left[i](0,0).imag() << " " << self_energy_right[i](0,0).real()  << " " << self_energy_right[i](0,0).imag() << std::endl;
+        }
+    
+        self_energies.close();
+        std::cout << "Self energies saved to a file succesfully\n";
+
+        std::fstream gamma("gamma.dat", std::ios::out);
+
+        if(!gamma)
+        {
+            std::system("touch gamma.dat");
+        }
+
+        if(!gamma)
+        {
+            std::cerr << "File gamma.dat was unable to open";
+            return 1;
+        }
+
+    
+        for(size_t i = 0; i<E.size(); i++)
+        {
+            gamma << E[i].real() << " " << gamma_left[i](0,0).real()  << " " << gamma_left[i](0,0).imag() << " " << gamma_right[i](0,0).real()  << " " << gamma_right[i](0,0).imag() << std::endl;
+        }
+    
+        gamma.close();
+        std::cout << "Gamma matrices saved to a file succesfully\n";
+
+        std::fstream hamiltonian_sample("hamiltonian_sample.dat", std::ios::out);
+
+        if(!hamiltonian_sample)
+        {
+            std::system("touch hamiltonian_sample.dat");
+        }
+
+        if(!hamiltonian_sample)
+        {
+            std::cerr << "File hamiltonian_sample.dat was unable to open";
+        }
+
+        for(size_t i = 0; i<xsize; i++)
+        {
+            for(size_t j = 0; j<xsize; j++)
             {
-                Greens_fun_retarded << GF_retarted[k](i, j) << " ";
+                hamiltonian_sample << H_sample(i, j) << " ";
             }
-            Greens_fun_retarded << std::endl;
+            hamiltonian_sample << std::endl;
+        }   
+
+        hamiltonian_sample.close();
+        std::cout << "Hamiltonian of the sample saved to a file succesfully\n";
+
+        std::fstream Greens_fun_retarded("Greens_fun_retarded.dat", std::ios::out);
+
+        if(!Greens_fun_retarded)
+        {
+            std::system("touch Greens_fun_retarded.dat");
         }
-    }
-    */
-   for(size_t k = 0; k< GF_retarted.size(); k++)
-   {
-        Greens_fun_retarded << E[k].real() << " " << GF_retarted[k](0, GF_retarted[k].n_cols-1).real() << " " << GF_retarted[k](0, GF_retarted[k].n_cols-1).imag() << std::endl;
-   }
+
+        if(!Greens_fun_retarded)
+        {
+            std::cerr << "File Greens_fun_retarded.dat was unable to open";
+        }
+        
+        for(size_t k = 0; k< GF_retarted.size(); k++)
+        {
+            Greens_fun_retarded << E[k].real() << " " << GF_retarted[k](0, GF_retarted[k].n_cols-1).real() << " " << GF_retarted[k](0, GF_retarted[k].n_cols-1).imag() << std::endl;
+        }
     
     
 
-    Greens_fun_retarded.close();
-    std::cout << "Retarded Greens function saved to a file succesfully\n";
+        Greens_fun_retarded.close();
+        std::cout << "Retarded Greens function saved to a file succesfully\n";
+    }
+    
 
-    std::fstream transp_fun("transp_fun.dat", std::ios::out);
+    std::fstream transm_fun("transm_fun.dat", std::ios::out);
 
-    if(!transp_fun)
+    if(!transm_fun)
     {
-        std::system("touch transp_fun.dat");
+        std::system("touch transm_fun.dat");
     }
 
-    if(!transp_fun)
+    if(!transm_fun)
     {
-        std::cerr << "File transp_fun.dat was unable to open";
+        std::cerr << "File transm_fun.dat was unable to open";
         return 1;
     }
 
     
     for(size_t i = 0; i<E.size(); i++)
     {
-        transp_fun << E[i].real() << " " << T[i].real() << " " << T[i].imag() << std::endl;
+        transm_fun << E[i].real() << " " << T[i].real() << " " << T[i].imag() << std::endl;
     }
     
-    transp_fun.close();
-    std::cout << "Transport function saved to a file succesfully\n";
+    transm_fun.close();
+    std::cout << "Transmission function saved to a file succesfully\n";
 
     std::fstream conductance("conductance.dat", std::ios::out);
 
@@ -372,46 +389,11 @@ int main()
     
     for(size_t i = 0; i<E.size(); i++)
     {
-        conductance << E[i].real() << " " << (e*e/h)*T[i].real() << " " << (e*e/h)*T[i].imag() << std::endl;
+        conductance << E[i].real() << " " << (e*e/h_planck)*T[i].real() << " " << (e*e/h_planck)*T[i].imag() << std::endl;
     }
     
     conductance.close();
     std::cout << "Conductance saved to a file succesfully\n";
-
-    std::fstream Greens_fun_advanced("Greens_fun_advanced.dat", std::ios::out);
-
-    if(!Greens_fun_advanced)
-    {
-        std::system("touch Greens_fun_advanced.dat");
-    }
-
-    if(!Greens_fun_advanced)
-    {
-        std::cerr << "File Greens_fun_advanced.dat was unable to open";
-    }
-    /*
-    for(size_t k = 0; k< GF_retarted.size(); k++)
-    {
-        Greens_fun_advanced << "advanced Green's function for E=" << E[k] << std::endl;
-        for(size_t i = 0; i<size; i++)
-        {
-            for(size_t j = 0; j<size; j++)
-            {
-                Greens_fun_advanced << GF_retarted[k](i, j) << " ";
-            }
-            Greens_fun_advanced << std::endl;
-        }
-    }
-    */
-   for(size_t k = 0; k< E.size(); k++)
-   {
-        Greens_fun_advanced << E[k].real() << " " << GF_retarted[k](0, GF_retarted[k].n_cols-1).real()*GF_advanced[k](GF_advanced[k].n_cols-1, 0).real() << " " << GF_retarted[k](0, GF_retarted[k].n_cols-1).imag()*GF_advanced[k](GF_advanced[k].n_cols-1, 0).imag() << std::endl;
-   }
-    
-    
-
-    Greens_fun_advanced.close();
-    std::cout << "advanced Greens function saved to a file succesfully\n";
 
     std::fstream LDOS("LDOS.dat", std::ios::out);
 
